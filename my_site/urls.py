4c47bebaf9d2b476284b6204b5c3e16b3886a4b0
Adrian88 <adrian88szymanski@gmail.com>
2021-03-12 19:06:41 +0100

    5 commit

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('', RedirectView.as_view(url='/blog/')),
 ]
